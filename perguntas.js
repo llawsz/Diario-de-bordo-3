8f2eaa0b21ec37458d5d86940dfd725cb814d373
llawsz <138492465+llawsz@users.noreply.github.com>
2024-11-26 20:12:15 -0300

    Update perguntas.js
    respondendo perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
     'Diário de Bordo', 
     'Qual foi o destino?', 
-    'Informar o destino da viagem.', 
+    'Dubai', 
 )
 
 criaCartao(
     'Custo da Viagem', 
     'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?', 
-    'Informar o valor na moeda original do local e o valor convertido para R$. Informar se a aquisição foi via agência de viagens ou aquisição de forma independente '
+    'O custo da viagem por pessoa para ficar 7 dias foi de R$12.317. A moeda original é o dirham, 1AED = 1,30BRl. A aquisição foi de forma independente. '
 )
 
 criaCartao(
     'Fato Curioso', 
     'Uma curiosidade sobre cidade/ponto turístico/país visitado!', 
-    'Informar através de um breve texto uma curiosidade.'
+    'Dubai é famosa por suas inovações, como a construção de ilhas artificiais e o investimento em tecnologia, além de ser lar de uma das maiores populações de expatriados do mundo. Mesmo com toda a modernidade, mantém tradições culturais, como a dança Al Ayala e a hospitalidade árabe.'
 )
 
 criaCartao(
     'Passeios', 
     'Onde passear?', 
-    'Informar os locais, breve resumo do que são.'
+    '2 lugares bem populares em Dubai são Downtown Dubai e Jumeirah. Em Downtown Dubai, Área moderna com marcos como o Burj Khalifa e Dubai Mall.Centro de compras e entretenimento. Em Jumeirah, é um bairro costeiro famoso por suas praias, resorts luxuosos e o Burj Al Arab.'
 )
 
 criaCartao(
     'Prato Típico', 
     'Conhece o "nome do prato"?', 
-    'Explicar sobre o prato, porque o escolheu e o ingrediente principal.'
+    'Al Harees é um prato árabe simples feito com trigo e carne (geralmente cordeiro ou frango). O trigo é cozido até se desintegrar, formando uma consistência cremosa, e a carne adiciona sabor.Ele é popular em ocasiões especiais, como o Ramadã. O ingrediente principal é o trigo, que dá ao prato sua textura única'
 )
 
 criaCartao(
     'Ponto de Vista', 
     'O que eu achei da viagem para ...?', 
-    'Informar através de um breve texto sobre o que achou da viagem, o que faria de diferente e por quê.'
+    'A viagem para Dubai foi uma experiência incrível! A cidade é um verdadeiro contraste entre o moderno e o tradicional, com seus arranha-céus impressionantes e mercados tradicionais.uma coisa que faria de diferente seria reservar mais tempo para explorar as áreas culturais e menos turísticas, como o bairro histórico de Al Fahidi e os mercados locais.'
 )
